f65ef3eac89e30b12d882af3ea41906cdbfbf3d1
Grigi <nagrigoriadis@gmail.com>
2012-06-27 12:21:01 +0200

    Start of html5boilerplate stuff

--- app.sample/urls.py
+++ app.sample/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, include, url
-from . import APP_ROOT
 
 urlpatterns = patterns('app.views',
     # Examples:
     url(r'^$', 'home', name='home'),
 
-) + patterns('',
-    # Serve the apps static dir
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/static' % APP_ROOT}),
 )
 
--- project_name/urls.py
+++ project_name/urls.py
@@ -1,16 +1,23 @@
 from os.path import dirname,normpath,abspath
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
+from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Admin panel and documentation:
+    # Admin panel and documentation
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^static/admin/(?P<path>.*)$', 'django.views.static.serve', {'document_root': '%s/static/admin' % dirname(normpath(abspath(admin.__file__)))}),
-    # Examples:
+
+    # Examples
     # url(r'^app/', include('app.urls')),
 
 )
+
+# Serve static files only if DEBUG=True
+# See: https://docs.djangoproject.com/en/dev/howto/static-files/
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
+
