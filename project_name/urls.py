f9ccdaa6f3aea2f13d0264ec21dcab3cd7a2adf6
Grigi <nagrigoriadis@gmail.com>
2012-06-27 16:23:48 +0200

    Serving some root files aswell

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,4 +1,4 @@
-from os.path import dirname,normpath,abspath
+from os.path import dirname,normpath,abspath,join
 from django.conf.urls import patterns, include, url
 from django.conf import settings
 
@@ -20,5 +20,11 @@ urlpatterns = patterns('',
 # See: https://docs.djangoproject.com/en/dev/howto/static-files/
 if settings.DEBUG:
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    staticroot = normpath(join(settings.PROJECT_ROOT, '{{ project_name }}/static'))
     urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += patterns('',
+        (r'^favicon\.ico$', 'django.views.static.serve', { 'document_root': staticroot, "path":'favicon.ico' }),
+        (r'^robots\.txt$', 'django.views.static.serve', { 'document_root': staticroot, "path":'robots.txt' }),
+        (r'^crossdomain\.xml$', 'django.views.static.serve', { 'document_root': staticroot, "path":'crossdomain.xml' }),
+    )
 
