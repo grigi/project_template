42627a3f948072d25d2603759813cf42f8117b4f
Grigi <nagrigoriadis@gmail.com>
2012-07-03 14:18:46 +0200

    local settings support

--- manage.py
+++ manage.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import os
 import sys
+import imp
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.dev")
+    settings_path = "{{ project_name }}.settings.dev"
+    try:
+        # Check for local settings.py in local directory.
+        imp.find_module('local_settings')
+        settings_path = "local_settings"
+    except:
+        # No local settings.py file
+        pass
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_path)
 
     from django.core.management import execute_from_command_line
 
