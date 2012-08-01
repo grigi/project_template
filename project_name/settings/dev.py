5c0ebc02b4a5b2ac8327f5f8300698c102a7f6b3
Grigi <nagrigoriadis@gmail.com>
2012-08-01 22:28:57 +0200

    Added django-compressor

--- project_name/settings/dev.py
+++ project_name/settings/dev.py
@@ -23,6 +23,12 @@ DATABASES = {
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = '{{ project_name }}.wsgi_dev.application'
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = normpath(join(PROJECT_ROOT, 'static_root'))
+
 INSTALLED_APPS += (
     # Any dev-only apps to include
 )
